d6b128a1f9ee6f8b365c41055967b3b003263b30
morrisquan <2689579071@qq.com>
2019-06-21 20:06:58 +0800

    Squashed commit of the following:
    commit 0812c812f9f0565fb31bc144eb15a339aaea128a
    Author: gaoxi217 <1096928980@qq.com>
    Date:   Fri Jun 21 19:53:15 2019 +0800
    
        gaoxi+a
    
    commit 23c3b20d327f8ddf8062864a8cfd5019f53edb13
    Author: gaoxi217 <1096928980@qq.com>
    Date:   Fri Jun 21 19:36:15 2019 +0800
    
        gaoxi
    
    # Conflicts:
    #	SiJieDuanDao/src/main/java/com/qphone/testdao/Test1.java
    #	SiJieDuanDao/target/classes/com/qphone/testdao/Test1.class

--- SiJieDuanDao/src/main/java/com/qphone/testdao/Test1.java
+++ SiJieDuanDao/src/main/java/com/qphone/testdao/Test1.java
@@ -1,6 +1,7 @@
 package com.qphone.testdao;
 
 public class Test1 {
+<<<<<<< HEAD
 	
 	public static void main(String[] args) {
 		
@@ -13,4 +14,8 @@ public class Test1 {
 	
 	
 
+=======
+
+	private String a;
+>>>>>>> 0812c812f9f0565fb31bc144eb15a339aaea128a
 }
